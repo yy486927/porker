037268de48d3566794665c3f50de1d48ad4bf333
yoshimura <yoshimura-yuki@jp.fujitsu.com>
2019-02-07 14:10:10 +0900

    トランプJSONに。

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
   startDisable:boolean = false;
   initCounter:number = 0;
+  trump: Array<any> = [];
   
 
   constructor(public navCtrl: NavController) {
@@ -27,6 +28,38 @@ export class HomePage {
   drow(){
     this.initCounter++;
   }
+
+
+  cardset(){
+    this.json('♦');
+    this.json('♥');
+    this.json('♧');
+    this.json('♠');
+    console.log("数字" + this.trump[39].number);
+    console.log("記号" + this.trump[39].mark);
+  }
+ 
+  json(mark){
+    for(var i = 1; i < 11; i++){
+      for(var j = mark; j.length < i ; j += mark);
+       let addData = {
+        "number": i,
+        "mark": j
+      }
+      this.trump.push(addData);
+    }
+  }
+
+  random(){
+    
+  }
+
   
+ 
+  
+
 }
+
+
+
 
